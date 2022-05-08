9b2cf75052eb160ada9ca1ee0ef880506196297e
Michel <micheldavrdz@gmail.com>
2022-05-08 02:27:39 -0500

    add: FizzbuzzService and test
    add FizzbuzzService class and its jest test

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const Reader = require('./lib/utils/Reader');
 const ExplorerService = require('./lib/services/ExplorerService');
+const FizzbuzzService = require('./lib/services/FizzbuzzService');
 
 // Get the explorers data from the file
 const explorers = Reader.readJsonfile('./explorers.json');
@@ -9,3 +10,16 @@ const explorers = Reader.readJsonfile('./explorers.json');
 console.log(ExplorerService.filterByMission(explorers, 'node'));
 console.log(ExplorerService.getAmountOfExplorersByMission(explorers, 'node'));
 console.log(ExplorerService.getExplorersUsernamesByMission(explorers, 'node'));
+
+// Using FizzbuzzService to get trick values for the explorers
+const explorer1 = {name: "Explorer1", score: 1}
+FizzbuzzService.applyValidationInExplorer(explorer1) // {name: "Explorer1", score: 1, trick: 1} 
+
+const explorer3 = {name: "Explorer3", score: 3}
+FizzbuzzService.applyValidationInExplorer(explorer3) // {name: "Explorer3", score: 3, trick: "FIZZ"}
+
+const explorer5 = {name: "Explorer5", score: 5}
+FizzbuzzService.applyValidationInExplorer(explorer5) // {name: "Explorer5", score: 5, trick: "BUZZ"}
+
+const explorer15 = {name: "Explorer15", score: 15}
+FizzbuzzService.applyValidationInExplorer(explorer15) // {name: "Explorer15", score: 15, trick: "FIZZBUZZ"}
